2b23afcd1c9feb80d255242336752bc3ad48ab64
Mateusz Marzecki <mateusz.marzecki@gmail.com>
2020-04-28 19:44:49 +0200

    Added svelte and svelte-loader + webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('111')
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin()
   ],
+  resolve: {
+    // see below for an explanation
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        exclude: /node_modules/,
+        use: 'svelte-loader'
+      },
+    ]
+  }
 };
