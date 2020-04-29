7240961b725868706f4d1800ce127bc2be4b8561
Mateusz Marzecki <mateusz.marzecki@gmail.com>
2020-04-29 21:54:06 +0200

    Added Facebook component

---
+++ src/App/Buttons/FacebookSignIn/FacebookSignIn.svelte
@@ -0,0 +1,52 @@
+<script>
+import FacebookLogo from '../../../../assets/facebook_logo.png';
+</script>
+
+<div class="button">
+    <div class="button__wrapper">
+        <div class="button__element">
+            <div class="button__logo">
+            <!-- {@html SvgLogo} -->
+                <img src={FacebookLogo} alt="Facebook" />
+            </div>
+        </div>
+        <div class="button__content">
+            <span>Log in With Facebook</span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .button {
+        color: #fff;
+        background-color: #1877f2;
+        height: 48px;
+        cursor: pointer;
+        margin-top: 1.5em;
+        border-radius: 4px;
+    }
+
+    .button .button__wrapper {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .button__element {
+        width: 60px;
+        height: 48px;
+        padding: 0;
+    }
+
+    .button__content {
+        width: 100%;
+        font-family: Helvetica, Arial, sans-serif;
+        font-weight: 700;
+        line-height: 50px
+    }
+
+    .button__element .button__logo {
+        padding: 12px 14px;
+        height: 24px;
+        float: left;
+    }
+</style>
