e0610a4e4edf408822c0c097a8ae0343ef2c23a8
Mateusz Marzecki <mateusz.marzecki@gmail.com>
2020-05-15 00:57:03 +0200

    WIP on WeightContent

--- src/App/Main/Weight/WeightContent/WeightContent.svelte
+++ src/App/Main/Weight/WeightContent/WeightContent.svelte
@@ -1,13 +1,50 @@
 <script>
+export let extendedView;
+
 </script>
 
 <div class="wrapper">
-ddds
+    <div class="header">
+        <div>Date</div>
+        <div>Kg</div>
+        {#if extendedView}
+            <div>Fat</div>
+            <div>TWB</div>
+            <div>Muscle</div>
+            <div>Bone</div>
+        {/if}
+    </div>
+    <div class="entry">
+        <div class=""></div>
+    </div>
+    <div class="entry">
+    </div>
+    <div class="entry">
+    </div>
 </div>  
 
 <style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        position: relative;
+        padding-bottom: 4%;
+        font-weight: 700;
+        color: #f09;
+    }
 
-.weight__entry-container {
+    .header::after {
+        content: ' ';
+        position: absolute;
+        width: 80%;
+        height: 1px;
+        background-color: #f0c;
+        display: flex;
+        left: 10%;
+        bottom: 0;
+    }
+
+/* .weight__entry-container {
         padding: 1%;
         border-top: 1px solid #f1a;
         display: flex;
@@ -18,5 +55,5 @@ ddds
         font-size: 12px;
         color: #411;
         align-self: flex-end;
-    }
+    } */
 </style>
